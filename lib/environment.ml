7e219a2846b7068b37f19db995ac558e7089840f
Denis Fouchard <dffouchard@gmail.com>
2026-09-07 23:41:39 +0200

    fix: deleted local scope on variable assignment

--- lib/environment.ml
+++ lib/environment.ml
@@ -9,11 +9,31 @@ module Environment = struct
     |Scope of {outside:t; local:t}
 
   let empty = Global VarTable.empty
+
   let rec define (env: t) (name:string) (value:type_t) =
     match env with
     | Global env_t ->Global (VarTable.add  name value env_t)
     | Scope {outside=env_out;local=env_t} ->
       Scope {outside=env_out;local=(define env_t name value)}
+
+  let rec mutate (env:t) (name:string) (new_value:type_t) =
+    match env with
+    | Global env_t ->
+      (match VarTable.find_opt name env_t with
+      |None -> None
+      |Some res -> Some (Global (VarTable.add name new_value env_t))
+      )
+    | Scope {outside=env_out;local=env_t} ->
+      let mutate_in_local = mutate env_t name new_value in
+      (match mutate_in_local with
+        | None -> let try_outer_scope = mutate env_out name new_value in
+          (match try_outer_scope with
+          |None -> None
+          |Some new_env -> Some (Scope {outside=new_env;local=env_t})
+          )
+        |Some new_env ->  Some (Scope {outside=env_out;local=new_env})
+      )
+
 
   let rec get_var (env:t) (name:string) =
     match env with
